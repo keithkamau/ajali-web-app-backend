a0d261d4b04ee3c611d313366be49e34e3f4dde6
Keith Kamau <keith.kamau2@student.moringaschool.com>
2026-08-28 18:21:13 +0300

    fix: add health_check view

--- core/views.py
+++ core/views.py
@@ -5,5 +5,6 @@ from django.views.decorators.csrf import csrf_exempt
 def health_check(request):
     return JsonResponse({
         "status": "healthy",
-        "message": "Ajali! API is running"
+        "message": "Ajali! API is running",
+        "version": "1.0.0"
     })
